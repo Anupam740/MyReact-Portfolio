c8ecda177cc7c8bc3f59e93b3d3363af95c6006b
Anupam740 <anupamkumar740@gmail.com>
2023-07-05 13:55:04 +0530

    minor changes

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div
+    name="about"
+    className="w-full h-fit bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full"> 
+        <div className="pb-8 mb-5">
+           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            ABOUT
+            </p>
+        </div>
+        <p className='text-xl mt-6'>
+        Step into the realm of innovation and creativity, where the digital canvas comes to life through the hands of a skilled frontend developer. It is with great pleasure that I introduce myself as a student of React JS, wielding a year of immersive experience and a passion for crafting seamless user experiences.</p>
+
+        <p className='text-l mt-5'><span className='text-3xl'>Greetings!</span> I am a dedicated individual who has ventured into the vast realm of computer science, pursued the art of engineering. As destiny would have it, my journey led me to Function Up, where I proudly serve as a React Developer Trainee, honing my skills and pushing the boundaries of what is possible in the digital landscape.In my tenure as a frontend developer, React JS has become my trusted companion, enabling me to breathe life into remarkable projects that span various industries and purposes. With an arsenal of Redux, Recoil, and Context API at my disposal, I gracefully orchestrate the symphony of state management, allowing applications to flourish with unparalleled efficiency and robustness.The canvas of my accomplishments boasts an array of captivating projects, each meticulously crafted with React JS as the cornerstone. From breathtaking user interfaces that mesmerize with their elegance to dynamic web applications that streamline complex tasks, I have had the privilege of weaving the threads of code into tangible creations that leave a lasting impact.</p>
+        <p className='text-l mt-5'>Yet, my journey extends beyond the realm of code. I believe that the true essence of frontend development lies in understanding the user, empathizing with their needs, and crafting experiences that resonate with their hearts and minds. By blending technical prowess with a keen eye for design and a deep appreciation for human interaction, I strive to create intuitive interfaces that engage, inspire, and elevate the digital landscape.As we embark on this journey, together we will push the boundaries of possibility, transcending the ordinary to create extraordinary digital experiences. With React JS as our vessel and my passion as our guiding light, we will unlock the true potential of the web, one line of code at a time.</p>
+        <p className='text-l mt-5'>So join me on this exciting adventure, where innovation and creativity converge, and let us transform the digital landscape into a realm of awe and wonder. Together, we will forge a path towards excellence, fueled by the power of React JS and driven by our shared passion for frontend development. Welcome to a world where imagination knows no bounds!</p>
+
+
+
+
+
+
+       
+
+
+        
+        <br />
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default About
+
